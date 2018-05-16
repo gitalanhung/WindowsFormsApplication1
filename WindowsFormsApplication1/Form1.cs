8d1d8e08898c0d5ff8589a4fd9c2194463c0c353
庆祝 孔 <alanhung@sina.com>
2018-05-16 16:22:03 +0800

    old master

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,5 +20,16 @@ namespace WindowsFormsApplication1
             //MessageBox.Show("");
             //jjjjjjjjjjjjjjjjjjjj
         }
+
+        List<int> dic = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+                dic.Add(i);
+
+            dic.ForEach(i => i += 10);
+            foreach (int i in dic)
+                Console.WriteLine(i.ToString());
+        }
     }
 }
